084261805a5bea4e83517548690411480d53da07
Rom <romulo.santos@travelperk.com>
2021-10-20 22:10:39 +0200

    adds core app

---
+++ app/core/tests/tests_model.py
@@ -0,0 +1,43 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTests(TestCase):
+
+    def test_create_user_with_email_successful(self):
+        email = 'email@domain.com'
+        password = 'aPassword'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_create_user_with_normalized_domain_name(self):
+        email = 'anotheremail@DomAin.com'
+        password = 'aPassword'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+
+        self.assertEqual(user.email, email.lower())
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_with_invalid_email_raises_error(self):
+        with self.assertRaises(ValueError):
+            email = ''
+            password = 'aPassword'
+            get_user_model().objects.create_user(
+                email=email,
+                password=password
+            )
+
+    def test_create_new_super_user(self):
+        user = get_user_model().objects.create_superuser(
+            'address@domain.com', 'aPassword'
+        )
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
