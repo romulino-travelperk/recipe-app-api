df546a5952953cdea608fbffa60ba29f32ed5f9b
Rom <romulo.santos@travelperk.com>
2021-10-23 19:06:34 +0200

    adds tags

--- app/core/tests/tests_model.py
+++ app/core/tests/tests_model.py
@@ -1,6 +1,12 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+from core import models
+
+
+def create_sample_user(email='some@email.com', password='apassword123'):
+    return get_user_model().objects.create_user(email, password)
+
 
 class ModelTests(TestCase):
 
@@ -41,3 +47,10 @@ class ModelTests(TestCase):
         )
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+    def test_tag_string_representation(self):
+        tag = models.Tag.objects.create(
+            user=create_sample_user(),
+            name='Vegan'
+        )
+        self.assertEqual(str(tag), tag.name)
